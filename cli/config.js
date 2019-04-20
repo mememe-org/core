38fbcab0af61a585573f9e79a70ad6ca366384ea
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2019-04-20 23:59:53 +0700

    remove cli folder

--- cli/config.js
+++
@@ -1,7 +0,0 @@
-const { homedir } = require('os')
-const path = require('path')
-
-module.exports = {
-  templatesPath: path.join(homedir(), '.mememe/templates'),
-  hubURL: 'http://meme.connek.tk'
-}
