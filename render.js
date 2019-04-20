94358a96c20411ba942078837320237fc9a910a3
blead <blead010@gmail.com>
2019-04-20 16:04:51 +0700

    Try to fix render

--- render.js
+++ render.js
@@ -4,21 +4,23 @@ const path = require('path')
 const { LATEST_VERSION } = require('./defaults')
 
 const renderTextElement = (element, canvas) => {
-  const { text, font, textAlign, textBaseline, direction, color, stroke, transform, position } = element
-  const ctx = canvas.getContext('2d')
-  ctx.font = font
-  ctx.textAlign = textAlign
-  ctx.textBaseline = textBaseline
-  ctx.direction = direction
-  ctx.fillStyle = color
-  ctx.fillText(text, position.x, position.y)
+  return new Promise(resolve => {
+    const { text, font, textAlign, textBaseline, direction, color, stroke, transform, position } = element
+    const ctx = canvas.getContext('2d')
+    ctx.font = font
+    ctx.textAlign = textAlign
+    ctx.textBaseline = textBaseline
+    ctx.direction = direction
+    ctx.fillStyle = color
+    ctx.fillText(text, position.x, position.y)
 
-  if (stroke !== undefined) {
-    ctx.lineWidth = stroke.width
-    ctx.strokeStyle = stroke.color
-    ctx.strokeText(text, position.x, position.y)
-  }
-  return Promise.resolve()
+    if (stroke !== undefined) {
+      ctx.lineWidth = stroke.width
+      ctx.strokeStyle = stroke.color
+      ctx.strokeText(text, position.x, position.y)
+    }
+    resolve()
+  })
 }
 
 const renderImageElement = (element, canvas) => {
@@ -36,7 +38,7 @@ const render = async (spec, canvas) => {
     canvas.width = size.width
     canvas.height = size.height
 
-    await Promise.all(Object.values(elements)
+    await Object.values(elements)
       .sort((a, b) => {
         if (a.z < b.z) {
           return -1
@@ -46,14 +48,15 @@ const render = async (spec, canvas) => {
         }
         return 1
       })
-      .map(value => {
-        if (value.type === 'text') {
-          return renderTextElement(value, canvas)
+      .reduce((prev, element) => {
+        if (element.type === 'text') {
+          return prev.then(renderTextElement(element, canvas))
         }
-        if (value.type === 'image') {
-          return renderImageElement(value, canvas)
+        if (element.type === 'image') {
+          return prev.then(renderImageElement(element, canvas))
         }
-      }))
+        return prev
+      }, Promise.resolve())
     return canvas
   }
   throw new Error('Unsupported version')
