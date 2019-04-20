d6eae477072d6b7d638ececc012ae4cc633ab076
blead <blead010@gmail.com>
2019-04-20 21:04:04 +0700

    Add new image transformers

--- transformers/image.js
+++ transformers/image.js
@@ -1,9 +1,18 @@
 const findEyes = require('../helpers/findEyes')
 const { flareEyes } = require('../helpers/flareEyes')
+const { sunGlasses: sunglasses, eyeBlock: eyeblock } = require('../helpers/sunGlasses');
 
 module.exports = {
   hyperify: (image) => {
     return findEyes.run(image)
       .then(results => flareEyes(results, image))
+  },
+  sunglasses: (image) => {
+    return findEyes.run(image)
+      .then(results => sunglasses(results, image))
+  },
+  eyeblock: (image) => {
+    return findEyes.run(image)
+      .then(results => eyeblock(results, image))
   },
 }
