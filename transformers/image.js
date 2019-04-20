1c665b41b3111a583ec33addd7c76aa5da2fbff2
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2019-04-21 00:04:45 +0700

    image transformers are now loaded only when a special flag is supported, to reduce dependency on canvas module

--- transformers/image.js
+++ transformers/image.js
@@ -1,18 +1,22 @@
-const findEyes = require('../helpers/findEyes')
-const { flareEyes } = require('../helpers/flareEyes')
-const { sunGlasses: sunglasses, eyeBlock: eyeblock } = require('../helpers/sunGlasses');
+if(process.env.IMAGE_TRANSFORMER_ENABLED) {
+  const findEyes = require('../helpers/findEyes')
+  const { flareEyes } = require('../helpers/flareEyes')
+  const { sunGlasses: sunglasses, eyeBlock: eyeblock } = require('../helpers/sunGlasses');
 
-module.exports = {
-  hyperify: (image) => {
-    return findEyes.run(image)
-      .then(results => flareEyes(results, image))
-  },
-  sunglasses: (image) => {
-    return findEyes.run(image)
-      .then(results => sunglasses(results, image))
-  },
-  eyeblock: (image) => {
-    return findEyes.run(image)
-      .then(results => eyeblock(results, image))
-  },
+  module.exports = {
+    hyperify: (image) => {
+      return findEyes.run(image)
+        .then(results => flareEyes(results, image))
+    },
+    sunglasses: (image) => {
+      return findEyes.run(image)
+        .then(results => sunglasses(results, image))
+    },
+    eyeblock: (image) => {
+      return findEyes.run(image)
+        .then(results => eyeblock(results, image))
+    },
+  }
+} else {
+  module.exports = {}
 }
